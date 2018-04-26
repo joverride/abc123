e86c6fa4bc2712ecb26aed8186cdbdc8df7f9918
JOverride <alessandrobruno.daniele@gmail.com>
2018-04-26 20:00:02 +0200

    Entities

--- BaseProject/src/main/java/core/data/UserOperations.java
+++ BaseProject/src/main/java/core/data/UserOperations.java
@@ -1,37 +1,33 @@
 package core.data;
 
-import javax.naming.InitialContext;
+//import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import core.HibernateUtils;
+import entities.Utente;
 
 public class UserOperations {
 
 	public static void test() throws NotSupportedException, SystemException, HeuristicMixedException, HeuristicRollbackException, RollbackException, NamingException {
-		UserTransaction tx = null;
+		SessionFactory factory = HibernateUtils.getInstance();
+	    Session s =  factory.getCurrentSession();
 		try {  
-		     tx = (UserTransaction)new InitialContext()  
-		                            .lookup("java:comp/UserTransaction");  
-		    tx.begin();  
-		    SessionFactory factory = HibernateUtils.getInstance();
-		    // Do some work  
-		    Session s =  factory.getCurrentSession();
-		  //  Utente u = s.get(Utente.class,new Integer(2));  
-		    System.out.println(tx.toString());
-		    System.out.println(s.getTransaction().toString());
-		    tx.commit();  
+		    if (!s.getTransaction().isActive())
+				s.beginTransaction();
+		    Utente u = s.get(Utente.class,new Integer(2));  
+		    System.out.println(u == null);
+		    s.getTransaction().commit();
 		}  
 		catch (RuntimeException e) {  
-		    tx.rollback();  
+			s.getTransaction().rollback();
 		    throw e; // or display error message  
 		} 
 	}
